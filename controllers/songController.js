103e8b32086ed2bfec4ac3b79bab58e20f25a708
qjoshlawlor <joshlawlor1@gmail.com>
2022-06-13 20:04:58 -0400

    DELETE WORKS!

--- controllers/songController.js
+++ controllers/songController.js
@@ -8,12 +8,9 @@ let create = (req,res)=>{
         if(err){
             res.status(400).json(err)
         }
-        console.log(song.id)
         Playlist.findById(req.params.playlistID, (err,pl)=>{
             pl.songs.push(song)
             pl.save()
-            // console.log(pl)
-            // console.log(pl.songs)
             res.render('show', {name: pl.name, info: pl.info, playlistID: req.params.playlistID, songs: pl.songs })    })
         })
         
@@ -22,27 +19,18 @@ let create = (req,res)=>{
 
 
 let show = (req,res)=>{
-    console.log(`Song show route, req ID:${req.params.id}`) // Req.params.id is finding the Song ID not the Playlist ID
-    //Need to find song id and display information
     Song.findById(req.params.id, (err,song)=>{
         if(err){
             res.status(400).json(err)
             return
         }
         res.render('songs', {name: song.name, artist: song.artist, genre: song.genre, rating: song.rating, playlistID: req.params.playlistID, songID: req.params.id })
-    })
-    // console.log(Playlist.songs)
-    
-
+    })    
 }
 
 let update = (req,res)=>{
     Playlist.findById(req.params.playlistID, (err,pl)=>{
-        console.log(`*******START*********`)
         const mySong = pl.songs.findIndex(songs => songs._id.toString() === req.params.id)
-                //Find playlist that song you are updating is inside
-            
-        console.log(`Before update: ${mySong}`)
         Song.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err,song)=>{
             pl.songs.push(song)
             pl.songs.splice(mySong, 1)
@@ -52,21 +40,34 @@ let update = (req,res)=>{
                 
                 res.render('show', {name: pl.name, info: pl.info, playlistID: req.params.playlistID, songs: pl.songs })
                 //THIS WAS THE BUG FIX THAT I HAVE BEEN TRYING TO FIGURE OUT ALL
-                // res.render('songs', {name: mySong.name, artist: song.artist, genre: song.genre, rating: song.rating, playlistID: req.params.playlistID, songID: req.params.id 
-                // })  
         })
         
     })
 }
 
 let destroy = (req,res)=>{
-    // Playlist.findByIdAndDelete(req.params.id, (err,pl)=>{
-    //     if(err){
-    //         res.status(400).json(err)
-    //         return
-    //     }
-    //     res.redirect('/playlists')
-    // })
+   Playlist.findById(req.params.playlistID, (err,pl)=>{
+        if(err){
+            res.status(400).json(err)
+            return
+        }
+        console.log(`****START****`)
+        console.log(req.params.id)
+    const mySongIndex = pl.songs.findIndex(songs => songs._id.toString() === req.params.id)
+    const mySong = pl.songs.find(songs => songs._id.toString() === req.params.id)
+    Song.findByIdAndDelete(req.params.id, (err,song)=>{
+        if(err){
+            res.status(400).json(err)
+            return
+        }
+        pl.songs.splice(mySongIndex, 1)
+        pl.save()
+        console.log('Saved')
+        res.render('show', {name: pl.name, info: pl.info, playlistID: req.params.playlistID, songs: pl.songs })
+        console.log(`****END****`)
+
+    })
+})
 }
     module.exports = {
         create,
